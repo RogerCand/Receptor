5476755db6e5a44ab64ccf143d630964503a2dc9
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2024-12-05 19:14:27 -0300

    prova

---
+++ RogerReceptorMicroservico/src/main/java/Prova/RogerReceptorMicroservico/RogerReceptorMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package Prova.RogerReceptorMicroservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogerReceptorMicroservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RogerReceptorMicroservicoApplication.class, args);
+	}
+
+}
---
+++ RogerReceptorMicroservico/src/test/java/Prova/RogerReceptorMicroservico/RogerReceptorMicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package Prova.RogerReceptorMicroservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogerReceptorMicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
